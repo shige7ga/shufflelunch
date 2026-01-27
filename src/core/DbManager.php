bdf04d45fefd04aaa01743c886e4cfa8b42ad345
Shige <44537062+shige7ga@users.noreply.github.com>
2026-01-27 16:41:15 +0900

    Uncomment dotenv loading in DbManager.php

--- src/core/DbManager.php
+++ src/core/DbManager.php
@@ -1,8 +1,8 @@
 <?php
 
-// require __DIR__ . '/../vendor/autoload.php';
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-// $dotenv->load();
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 
 class DbManager
 {
